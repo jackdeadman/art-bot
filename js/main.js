f66ab54a021f79d1e76183e7db6939e18fc7c2f7
Jack Deadman <jdeadman1@sheffield.ac.uk>
2018-08-22 23:47:22 +0100

    new ui concepts basic implementation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,46 @@
 var noOp = function() {};
 
+function drawArrowhead(context, angle, to, radius) {
+	var x_center = to.x;
+	var y_center = to.y;
+
+	var x;
+	var y;
+
+	context.beginPath();
+
+	x = radius * Math.cos(angle) + x_center;
+	y = radius * Math.sin(angle) + y_center;
+
+	context.moveTo(x, y);
+
+	angle += (1.0/3.0) * (2 * Math.PI)
+	x = radius * Math.cos(angle) + x_center;
+	y = radius * Math.sin(angle) + y_center;
+
+	context.lineTo(x, y);
+
+	angle += (1.0/3.0) * (2 * Math.PI)
+	x = radius *Math.cos(angle) + x_center;
+	y = radius *Math.sin(angle) + y_center;
+
+	context.lineTo(x, y);
+
+	context.closePath();
+
+	context.fill();
+}
+
+function radians2degrees(radians) {
+  radians = parseFloat(radians);
+  return radians * (180 / Math.PI);
+}
+
+function degrees2radians(degrees){
+  degrees = parseFloat(degrees);
+  return degrees * (Math.PI/180);
+}
+
 function DrawingPanel(context) {
   this.context = context;
   this.points = [];
@@ -34,10 +75,13 @@ function ArtBot(position, context) {
   this.position = position;
   this.context = context;
 
-  this.speed = 1;
+  this.speed = 0.5;
   this.turningSpeed = Math.PI / 100;
-  this.angle = 0;
+  this.turningSpeed = 0;
+  this.angle = Math.PI;
 };
+
+
 
 ArtBot.prototype.update = function() {
   this.angle = this.angle + this.turningSpeed;
@@ -49,15 +93,15 @@ ArtBot.prototype.update = function() {
 
 ArtBot.prototype.draw = function() {
   var context = this.context;
-  context.fillStyle = "red";
-  context.beginPath();
-  context.arc(this.position.x, this.position.y, 5, 0, 2 * Math.PI);
-  context.closePath();
-  context.fill();
+  context.fillStyle = "rgba(255, 0, 0, 0.5)";
+  drawArrowhead(context, this.angle, this.position, 10);
 };
 
-
 (function() {
+
+  var angleDisplay = document.getElementById('angle-display');
+  var angleText = document.getElementById('angle-text');
+  var menu = document.getElementById('menu');
 
   var canvas = document.getElementById('display');
   var context = canvas.getContext('2d');
@@ -66,9 +110,45 @@ ArtBot.prototype.draw = function() {
   canvas.setAttribute('height', canvas.offsetHeight);
 
   var panel = new DrawingPanel(context);
-  var robot = new ArtBot({x: 100, y: 100}, context);
+  var robot = new ArtBot({x: 500, y: 500}, context);
 
   var objects = [ panel, robot ];
+
+  document.querySelectorAll('.angle-changer').forEach(function(changer) {
+    changer.addEventListener('click', function() {
+      robot.angle += parseFloat(degrees2radians(changer.dataset.amount));
+    });
+  });
+
+  document.querySelectorAll('.angle-setter').forEach(function(changer) {
+    changer.addEventListener('click', function() {
+      robot.angle = parseFloat(degrees2radians(changer.dataset.amount));
+    });
+  });
+
+  document.querySelectorAll('.menu-button').forEach(function(menuButton) {
+    menuButton.addEventListener('click', function() {
+      menu.classList.remove('hidden');
+    });
+  });
+
+  var overlays = [].slice.call(document.querySelectorAll('.overlay'));
+  document.querySelectorAll('.menu-changer').forEach(function(menuChanger) {
+    menuChanger.addEventListener('click', function() {
+      menu.classList.add('hidden');
+      var indexChanging = parseInt(menuChanger.dataset.index);
+      overlays[indexChanging].classList.add('active');
+      overlays.forEach(function(overlay, index) {
+        if (index != indexChanging) {
+          overlay.classList.remove('active');
+        }
+      });
+    });
+  });
+
+
+  var angleDisplays = document.querySelectorAll('.angle-display');
+  var angleTexts = document.querySelectorAll('.angle-text');
 
   setInterval(function(){
     context.clearRect(0, 0, canvas.width, canvas.height);
@@ -77,7 +157,15 @@ ArtBot.prototype.draw = function() {
 
     panel.draw();
     robot.draw();
-  }, 1000/60);
+
+    angleDisplays.forEach(function(angleDisplay) {
+      angleDisplay.style.transform = 'rotate('+ robot.angle +'rad)';
+    });
+
+    angleTexts.forEach(function(angleText) {
+      angleText.innerText = (Math.round(radians2degrees(robot.angle)) % 360);
+    });
+  }, 1000/30);
 
 
 })();
